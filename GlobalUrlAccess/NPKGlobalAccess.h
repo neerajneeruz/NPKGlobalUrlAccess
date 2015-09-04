5b266e4cbceb0981f2b510c3a7776a430529e3ea
neerajneeruz <good4pc@gmail.com>
2015-09-04 11:53:22 +0530

    second

---
+++ GlobalUrlAccess/NPKGlobalAccess.h
@@ -0,0 +1,15 @@
+//
+//  NPKGlobalAccess.h
+//  GlobalUrlAccess
+//
+//  Created by MFluid Apps on 04/09/15.
+//  Copyright (c) 2015 Mfluid Mobile Apps Pvt. Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+extern NSString* const accessUrl;
+extern NSString* const accessString;
+int      const globalInteger = 23;
+@interface NPKGlobalAccess : NSObject
+
+@end
---
+++ GlobalUrlAccess/NPKGlobalAccess.m
@@ -0,0 +1,15 @@
+//
+//  NPKGlobalAccess.m
+//  GlobalUrlAccess
+//
+//  Created by MFluid Apps on 04/09/15.
+//  Copyright (c) 2015 Mfluid Mobile Apps Pvt. Ltd. All rights reserved.
+//
+
+#import "NPKGlobalAccess.h"
+NSString* const accessUrl=@"www.google.com";
+NSString* const accessString=@"globalString";
+int      const globalInteger;
+@implementation NPKGlobalAccess
+
+@end
